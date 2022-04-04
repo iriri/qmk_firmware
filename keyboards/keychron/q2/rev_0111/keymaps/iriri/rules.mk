2db9bd109801ca8a202381cc45b538f00612277b
iriri <iri@konnichiwastevenspielbergde.su>
2022-04-03 20:34:33 -0700

    ???

--- keyboards/keychron/q2/rev_0111/keymaps/iriri/rules.mk
+++
@@ -1,8 +0,0 @@
-EXTRAFLAGS += -Wno-attributes -O2
-
-GRAVE_ESC_ENABLE   = no
-LTO_ENABLE         = yes
-MAGIC_ENABLE       = no
-MOUSEKEY_ENABLE    = no
-SPACE_CADET_ENABLE = no
-UNICODE_ENABLE     = yes
