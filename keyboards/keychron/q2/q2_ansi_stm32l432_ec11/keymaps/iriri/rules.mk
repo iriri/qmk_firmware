ce97d1fdddb769c8819907cf42da9d87fe2f6ffa
iriri <iri@konnichiwastevenspielbergde.su>
2022-04-03 21:12:11 -0700

    encoder still kind of broken

---
+++ keyboards/keychron/q2/q2_ansi_stm32l432_ec11/keymaps/iriri/rules.mk
@@ -0,0 +1,8 @@
+EXTRAFLAGS += -Wno-attributes -O2
+
+GRAVE_ESC_ENABLE   = no
+LTO_ENABLE         = yes
+MAGIC_ENABLE       = no
+MOUSEKEY_ENABLE    = no
+SPACE_CADET_ENABLE = no
+UNICODE_ENABLE     = yes
--- keyboards/keychron/q2/q2_ansi_stm32l432_ec11/q2_ansi_stm32l432_ec11.c
+++ keyboards/keychron/q2/q2_ansi_stm32l432_ec11/q2_ansi_stm32l432_ec11.c
@@ -132,15 +132,7 @@ led_config_t g_led_config = {
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) { return false; }
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    return true;
+    return encoder_update_user(index, clockwise);
 }
 
 #endif
